1770642c28127aff5c494e59f97634cf8b403b2c
expert-git <expertgit@gmail.com>
2020-12-16 12:42:33 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CounterContainer from './containers/CounterContainer'
+import TodoListContainer from './containers/TodoListContainer'
+
+
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+      <br />
+      <TodoListContainer />
+    </div>
+  )
+}
+
+export default App;
